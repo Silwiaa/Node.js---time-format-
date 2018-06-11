7627d71298ca7189f6c4d86fdc32f65906d929e8
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-06-11 19:45:58 +0200

    add colors

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -15,12 +15,12 @@ function getOSinfo() {
         uptime = os.uptime(),
         userInfo = os.userInfo();
     
-    console.log('System:', type);
-    console.log('Release:', release);
-    console.log('CPU model:', cpu);
-    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
-    console.log('User name:', userInfo.username);
-    console.log('Home dir:', userInfo.homedir);
+    console.log('System:'.gray, type);
+    console.log('Release:'.red, release);
+    console.log('CPU model:'.blue, cpu);
+    console.log('Uptime: ~'.green, (uptime / 60).toFixed(0), 'min');
+    console.log('User name:'.cyan, userInfo.username);
+    console.log('Home dir:'.grey, userInfo.homedir);
 }
 
 //EXPORT getOSinfo
