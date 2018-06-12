6a451e97138f12b602f1e6f7956af4028d2bd9c4
Sylwia <35842428+Silwiaa@users.noreply.github.com>
2018-06-12 21:08:56 +0200

    add timeFormat=require..

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -1,5 +1,6 @@
-var os = require('os');
-var colors = require('colors');
+var os = require('os'),
+    colors = require('colors'),
+    timeFormat = require('modules/timeFormat.js');
 
 function getOSinfo() {
     var type = os.type();
