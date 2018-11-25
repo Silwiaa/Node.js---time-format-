207dd8b1316ff799b44ee550008efcaf71ce6704
Sylwia <35842428+Silwiaa@users.noreply.github.com>
2018-11-25 22:43:54 +0100

    update program.js

--- app/program.js
+++ app/program.js
@@ -1,28 +1,28 @@
-var os = require('os'),
-    OSinfo = require('../modules/OSinfo.js'),
-    
-// SET ENCODING UTF-8
+//IMPORT MODULES
+var OSinfo = require('../modules/OSinfo');
+
+//SET ENCODING UTF-8
 process.stdin.setEncoding('utf-8');
 
-
+//REACT FOR USER ACTION
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
-    if(input !==null) {
-        var instruction = input.trim();
-        switch(instruction) {
+    if (input !==null) {
+        var instruction = input.toString().trim();
+        switch (instruction) {
             case '/exit':
-                process.stdout.write('Quitting app\n');
+                process.stdout.write('Quiting app!\n');
                 process.exit();
                 break;
-            case '/sayhello':
-                process.stdout.write('hello!\n');
+            case '/version':
+                console.log(process.version);
+                console.log(process.env.LANG);
                 break;
             case '/getOSinfo':
                 OSinfo.print();
-                timeFormat.print();
                 break;
-            default:
+            default: 
                 process.stderr.write('Wrong instruction!\n');
-        };
+        }
     }
 });
