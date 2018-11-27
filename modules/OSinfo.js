d8a742b5630522d81f1ee8dce6002806d52d7129
Sylwia <35842428+Silwiaa@users.noreply.github.com>
2018-11-27 17:25:52 +0100

    update OSinfo.js

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -1,4 +1,5 @@
 var os = require('os'),
+    colors = require('colors'),
     timeFormat = require('./timeFormat');
 
 function getOSinfo() {
@@ -15,12 +16,12 @@ function getOSinfo() {
         uptime = os.uptime(),
         userInfo = os.userInfo();
     
-    console.log('System: ', type);
-    console.log('Release: ', release);
-    console.log('CPU model: ', cpu);
-    console.log('Uptime: ~ ', timeFormat.print(uptime));
-    console.log('User name: ', userInfo.username);
-    console.log('Home dir: ', userInfo.homedir);
+    console.log('System: '.yellow, type);
+    console.log('Release: '.orange, release);
+    console.log('CPU model: '.blue, cpu);
+    console.log('Uptime: ~ '.green, timeFormat.print(uptime));
+    console.log('User name: '.cyan, userInfo.username);
+    console.log('Home dir: '.grey, userInfo.homedir);
 }
 
 exports.print = getOSinfo;
